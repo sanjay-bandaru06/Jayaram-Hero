699e38d0fd1eb1f2bf9b866c754af8abb448be3a
Sanjay Bandaru <sanjay.bandaru565@gmail.com>
2026-04-03 13:24:45 +0530

    fix: use npx for vite build on Vercel and optimize Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase warning limit for large chunks
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        // Optional: split vendor libraries into separate chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
